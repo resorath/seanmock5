96dd1615c8767aff67eb55ac2056d751a4db3614
Sean Feil <sean@com.tophatandmonocle>
2014-02-26 23:14:18 -0700

    Infrastructure and initial thoughts.

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="The Portfolio of Sean Feil, Software Developer."
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The Portfolio of Sean Feil | Welcome!</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link href="css/local.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="content" id="front">
+      <div id="nav">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-3"><a href="#">Portfolio</a></div>
+            <div class="col-sm-3"><a href="#">Resume</a></div>
+            <div class="col-sm-3"><a href="#">Blog</a></div>
+            <div class="col-sm-3"><a href="#">Say Hello</a></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+
+        <div class="centre" id="greeting">
+          <h1>Hello!</h1>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-4" id="selfportrait">
+            <img src="img/me.jpg" alt="Self Portrait">
+          </div>
+
+          <div class="col-sm-8" id="selfblurb">
+            <p>My name is Sean, welcome! </p>
+            <p>I am a software developer specializing in web. My interests are; creating usable web applications, managing software projects and teaching to these paradigms.</p>
+            <p>To drill into my portfolio, please select one of my interest areas below.</p>
+          </div>
+
+        </div>
+
+
+      </div>
+    </div>
+
+    <div class="content" id="webux">
+      <div class="container">
+        <h1></h1>
+
+      </div>
+    </div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/com.seanfeil.js"></script>
+  </body>
+</html>
