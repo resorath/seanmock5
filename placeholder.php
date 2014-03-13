4af71a6f3b8b9c25c80a6504d14abc02bf96bfbb
Sean Feil <sean@com.tophatandmonocle>
2014-03-12 22:42:21 -0600

    soon!

---
+++ placeholder.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="The Portfolio of Sean Feil, Software Developer."
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The Portfolio of Sean Feil | Welcome!</title>
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Amaranth' rel='stylesheet' type='text/css'>
+    <link href="css/soon.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+      <div class="middlecontainer">
+        <div class="cent" id="greeting">
+          <h1>Soon &#0153;</h1>
+        </div>
+      </div>
+  </body>
+</html>
