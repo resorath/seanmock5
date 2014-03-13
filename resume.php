3d50930da04a1f430e7037c23bde8ebcff2f86c3
Sean Feil <sean@com.tophatandmonocle>
2014-03-12 22:31:59 -0600

    Resume

---
+++ resume.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Resume | Sean Feil</title>
+
+    <!--Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Amaranth' rel='stylesheet' type='text/css'>
+    <link href="css/resume.css" rel="stylesheet">
+
+
+    <!--HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <h1>Sean Feil</h1>
+        <p><script type='text/javascript'><!--
+              var v2="BJFVZNNJDCIGMEZX3";var v7=unescape("1/%278%1A%3D++*%25%2C.%21k97%5E");var v5=v2.length;var v1="";for(var v4=0;v4<v5;v4++){v1+=String.fromCharCode(v2.charCodeAt(v4)^v7.charCodeAt(v4));}document.write('<a href="javascript:void(0)" onclick="window.location=\'mail\u0074o\u003a'+v1+'?subject='+'\'">'+v1 +'<\/a>');
+            //--></script> | <a href="http://www.seanfeil.com">www.seanfeil.com</a></p>
+      </header>
+
+      <div id="keyskills" class="resume-table">
+        <p class="resume-leader">Key Skills</p>
+        <ul>
+          <li>Web software developer; proficient with modern languages, technologies, techniques and paradigms.</li>
+          <li>Software project manager; with a strong ability to transform business requirements into a tested, finished solution with focus on quality assurance and user acceptance testing.</li>
+          <li>User experience expert; working closely with business units to create usable, efficient user interfaces.</li>
+        </ul>
+
+      </div>
+
+      <div id="employmenthistory" class="resume-table">
+        <p class="resume-leader">Employment History</p>
+
+        <div id="teamlead">
+
+          <div class="row">
+            <div class="col-sm-9">
+              <p class="resume-title">Team Lead, Application Development - Development Services</p>
+            </div>
+
+            <div class="col-sm-3">
+              <p class="resume-date">February '13 - Current</p>
+            </div>
+          </div>
+
+          <p class="resume-location">University of Calgary, Calgary AB</p>
+
+          <ul>
+            <li>Currently acting as a project manager and business analyst for applications internal and external to development teams.</li>
+            <li>Implemented TELUS Frontline CRM for Integrated Service Centre team as a replacement to existing BMC Remedy. Working closely with TELUS counterparts, configured the product to business specifications and integrated the product into the University’s infrastructure; such as a seamless transition with BMC Remedy incidents, synchronization with contact records in PeopleSoft and exporting data to Tableau metrics reporting.</li>
+            <li>Implemented Atlas ARES copyright management suite, including integration with Blackboard learning management system and other University infrastructure. Coordinated with key business representatives from the Library and Copyright Office to ensure product was configured and operated under their existing workflow. </li>
+            <li>Planned migration of all custom .NET applications and their SQL Server databases under the ADDS portfolio to new more modern environments.</li>
+            <li>Ran a project to replace the job profile database used by Human Resources with a custom .NET solution provided by ADDS development team members. This project included all tasks from gathering requirements; maintaining a close relationship with the business to adapt to changing circumstances and execution of the implementation, including quality assurance and user acceptance testing.</li>
+            <li>Executed a number of minor projects and acted as a substitute project manager for a number of larger projects when necessary.</li>
+            <li>Acting as a mentor to ADDS software developers, with oversight over all ongoing operations. </li>
+          </ul>
+        </div>
+
+        <div id="softwaredeveloper">
+
+          <div class="row">
+            <div class="col-sm-9">
+              <p class="resume-title">Software Developer, Application Development – Development Services</p>
+            </div>
+
+            <div class="col-sm-3">
+              <p class="resume-date">December ’11 — Current</p>
+            </div>
+          </div>
+
+          <p class="resume-location">University of Calgary, Calgary AB</p>
+
+          <ul>
+            <li>Developed custom web solutions primarily in .NET working individually or in small groups.</li>
+            <li>Designed and implemented a complete overhaul of the UofC Active Living’s program and event registration system. This solution involved close relationship with the business to redesign their data model, provide a modern user interface through numerous user experience testing sessions and a robust and intuitive administration system for their staff. Finally, I ran multiple training sessions to familiarize the staff with our tag-based data model.</li> 
+            <li>Additionally for Active Living, redesigned their public authentication system across all their applications. As well, implemented a voucher and discount addition to their existing administrative backend to allow the business to offer advertising incentives to their customer base.</li>
+            <li>Implemented a new solution to replace an aging Cold Fusion-based application for Risk Management. This solution allows motor vehicle driver’s working for the University to register themselves. The solution also includes a complete workflow and auditing system to risk management staff and department approvers. </li>
+            <li>Contributed to the project which moved over 2000 Drupal 5 enabled websites to the new Drupal 7 infrastructure. This included setting up the server environment, writing automation scripts for use by the Web Content Management client services team and coordinate with University Relations group to coordinate efforts.</li>
+            <li>Contributed changes to numerous .NET applications including IT Software Management, UPASS distribution management and other internal applications to meet updated business requirements.</li>
+            <li>Continued oversight and upkeep of open source applications such as CAS, ActiveMQ, VirtualLDAP and others from previous role. </li>
+          </ul>
+
+        </div>
+
+        <div id="analyst">
+
+          <div class="row">
+            <div class="col-sm-9">
+              <p class="resume-title">Technology Solutions Analyst, Web Integration Services</p>
+            </div>
+
+            <div class="col-sm-3">
+              <p class="resume-date">October 2008 — December 2011</p>
+            </div>
+          </div>
+
+          <p class="resume-location">University of Calgary, Calgary AB</p>
+
+          <ul>
+            <li>Contributed to and maintained open source and proprietary applications that supported core University web infrastructure, acting as a software developer and server administrator.</li>
+            <li>Enhanced and supported the Central Authentication Service used by most key applications on campus, including supporting business units who implemented the CAS protocol and the client software necessary for their integration. Enhancements were comprised of adding authentication schemes, a variety of service validation points to meet client needs, core bug and vulnerability fixes, and stability improvements. </li>
+            <li>Ensured the smooth operation of the LDAP authentication and directory services infrastructure by maintaining the openLDAP and custom built VirtualLDAP applications.</li>
+            <li>Supported the RSA SecurID two-factor authentication used by the University. These duties included upgrading from version 6 to 7, migration of the application to more modern operating systems and maintaining the client applications dependent on two-factor authentication.</li>
+            <li>Developed a number of custom add-ons (building blocks) for the BlackBoard learning management system. These add-ons include a large media (video, audio and high resolution images) management system; which can transform media to a web friendly format and make the media available to specific courses. Other building blocks included exporting grades to PeopleSoft and course linking. </li>
+            <li>Designed, developed and implemented an open-source two-factor alternative based on Yubico’s Yubikey technology. This full stack implementation included a web service API for secure authentication calls, desktop application for configuring hardware tokens and an administrative suite for managing the system.</li>
+            <li>Setup, maintained and encouraged adoption of the Apache ActiveMQ message queuing application. This open source alternative was adopted for numerous applications relying on IBM WebSphere, as well as new development. The key advantages were significant cost savings, community support and product customization.</li>
+            <li>Numerous other smaller projects across a number of technologies around integration, automation and easing complex business processes.</li>
+
+
+          </ul>
+
+        </div>
+
+        <div id="itsc">
+
+          <div class="row">
+            <div class="col-sm-9">
+              <p class="resume-title">Analyst, Information Technologies Support Centre</p>
+            </div>
+
+            <div class="col-sm-3">
+              <p class="resume-date">August 2007 — October 2008</p>
+            </div>
+          </div>
+
+          <p class="resume-location">University of Calgary, Calgary AB</p>
+
+          <ul>
+            <li>Supported staff, students, faculty and researchers with complex issues across a number of University systems – including supporting the business processes of ERP applications.</li>
+            <li>Provided the role of student support and later, PeopleSoft support.</li>
+            <li>Developed a custom web application for scheduling and resource substitution for the Support Centre’s student staff. This resulted in reduced overhead in time management for both the students and their management. </li>
+
+
+
+          </ul>
+
+        </div>
+
+      </div>
+
+      <div id="education" class="resume-table">
+        <p class="resume-leader">Education</p>
+
+        <div id="university">
+
+          <div class="row">
+            <div class="col-sm-9">
+              <p class="resume-title">Bachelors of Science in Computer Science</p>
+            </div>
+
+            <div class="col-sm-3">
+              <p class="resume-date">September 2003 — December 2007</p>
+            </div>
+          </div>
+
+          <p class="resume-location">University of Calgary, Calgary AB</p>
+
+          <ul>
+            <li>Focus in Human Computer Interaction and Distributed Systems</li>
+          </ui>
+
+        </div>
+
+
+        <div id="itil">
+
+          <div class="row">
+            <div class="col-sm-9">
+              <p class="resume-title">ITIL Foundations, Version 3</p>
+            </div>
+
+            <div class="col-sm-3">
+              <p class="resume-date">December 2012</p>
+            </div>
+          </div>
+
+          <p class="resume-location">University of Calgary, Calgary AB</p>
+        </div>
+
+
+      </div>
+
+
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
